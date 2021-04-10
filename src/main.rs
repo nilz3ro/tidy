eb460da3207be8aa3532a8bd1a412517ff93e4ff
nilz3ro <austin.j.buddington@gmail.com>
2021-04-10 16:20:14 -0400

    my $#!7 is working correctly.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 use std::error::Error;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use tokio::{
     fs,
     sync::{mpsc, oneshot},
@@ -9,16 +9,28 @@ use tokio::{
 
 #[derive(Debug)]
 struct DirRequest {
-    pub dir_name: String,
-    pub sender: oneshot::Sender<String>,
+    pub dir_path: PathBuf,
+    pub sender: oneshot::Sender<PathBuf>,
     pub close: bool,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let mut tasks: Vec<_> = vec![];
-    // Get the path to the directory to sort. -- TODO: This will come in as a CLI arg.
-    let the_path = Path::new("./sort_me");
+    let mut tasks = vec![];
+    // The source directory - the directory that we want to sort.
+    // TODO: get this as a cli arg.
+    let source_dir = Path::new("./sort_me").to_path_buf();
+    // In the future we will support sorting strategies, but for now
+    // the only way to sort will be by file extension.
+    //
+    // So, we will need to know what the target dir for each extension is, and if it exists.
+    // If it doesn't exist, the dir_manager task can create it as requested.
+    //
+    // For now we will only support copying files to target directories that share a common root.
+    // In the future, the user will be able to configure the target directory for each collection in a sorting strategy.
+    //
+    // TODO:  get this as a cli arg.
+    let target_root_dir = Path::new("./sorted");
 
     let (tx, mut rx) = mpsc::channel(48);
 
@@ -45,18 +57,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
         while let Some(dir_request) = rx.recv().await {
             match dir_request {
                 DirRequest {
-                    dir_name,
+                    dir_path,
                     sender,
                     close: false,
                 } => {
-                    // If the dir name isn't in the existing_dirs set:
-                    // check if the dir exists.
-                    println!("got a request! {:?}", &dir_name);
-                    let _ = sender.send(String::from(&dir_name));
-                    existing_dirs.insert(dir_name);
+                    // 1. Try to find the path in the set.
+                    // 2. If the path is in the set, respond with the Ok(path) immediately.
+                    // 3. If the path is not in the set,
+                    // 3.1 Find out if the path exists, and confirm that it's a directory.
+                    // 3.2 If the path exists and it's a directory, add it to the set.
+                    // 3.3 If the path exists and it's not a directory. respond with Err.
+                    // 3.4 If the path does not exist, create it and add it to the set,
+                    // then respond with the Ok(path).
+                    //
+                    // println!("got a request! {:?}", &dir_path);
+                    let _ = sender.send(dir_path.to_owned());
+                    existing_dirs.insert(dir_path);
                 }
                 DirRequest { close: true, .. } => {
-                    println!("here's all of the dirs. {:?}", existing_dirs);
+                    // Perform shutdown task.
                     rx.close();
                 }
             }
@@ -64,18 +83,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
     });
 
     // Get the contents of the junk drawer directory as a stream.
-    let mut dir_stream = fs::read_dir(the_path).await?;
+    let mut dir_stream = fs::read_dir(source_dir).await?;
 
     // With each item from the stream
     while let Some(dir_entry) = dir_stream.next_entry().await? {
-        println!("processing dir entry!!! YAY! {:?}", dir_entry);
+        // println!("dir_entry path: {:?}", dir_entry.path());
+        // println!("dir_entry extension? {:?}", dir_entry.path().extension());
+        let tgd = target_dir_for_extension(&target_root_dir, dir_entry.path()).await;
+        println!(
+            "Theoretical target dir for {:?} is {:?}",
+            dir_entry.path(),
+            tgd
+        );
         let ttx = tx.clone();
         let t = task::spawn(async move {
-            let dir_name = dir_entry.file_name().into_string().unwrap();
+            let dir_path = dir_entry.path();
             let (req_tx, req_rx) = oneshot::channel();
 
             let dir_request = DirRequest {
-                dir_name,
+                dir_path,
                 sender: req_tx,
                 close: false,
             };
@@ -88,13 +114,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 .await
                 .expect("failed to receive data from dir manager");
 
-            println!("got response! {}", res);
+            // println!("got response! {:?}", res);
         });
 
         tasks.push(t);
     }
 
-    println!("Waiting for tasks...");
+    // println!("Waiting for tasks...");
     for t in tasks {
         t.await?;
     }
@@ -107,17 +133,29 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let (_tx, _rx) = oneshot::channel();
 
     tx.send(DirRequest {
-        dir_name: "".into(),
+        dir_path: "".into(),
         sender: _tx,
         close: true,
     })
     .await?;
 
-    println!("Waiting for dir manager.");
+    // println!("Waiting for dir manager.");
 
     dir_manager.await?;
 
     println!("done!");
 
     Ok(())
+}
+
+async fn target_dir_for_extension(tg: &Path, pth: PathBuf) -> Option<PathBuf> {
+    // let tp = Path::new(pth.extension()).to_owned();
+    // tg.join(tp)
+    match pth.extension() {
+        Some(ext) => {
+            let p = Path::new(ext).to_owned();
+            Some(tg.join(p).to_path_buf())
+        }
+        None => None,
+    }
 }
